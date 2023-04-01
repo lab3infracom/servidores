68d393882cb423724d6550e77647658a1cbe0512
Javier Cerino <j.cerino@uniandes.edu.co>
2023-03-31 22:24:16 -0500

    h

--- UDP/src/Cliente/Main.java
+++ UDP/src/Cliente/Main.java
@@ -1,19 +1,19 @@
 package Cliente;
 
-public class Main{
+import Cliente.ClientUDP;
+
+public class Main {
     public static void main(String[] args) {
-    String serverHost = "localhost"; // Replace with the actual server host name or IP address
-    int serverPort = 12345; // Replace with the actual server port number
-    int clientId = 1; // Replace with the actual client ID
-    int numConnections = 2; // Replace with the actual number of connections
-    
-    try {
-        ClientUDP client = new ClientUDP(serverHost, serverPort);
-        client.downloadFile(clientId, numConnections);
-    } catch (Exception e) {
-        e.printStackTrace();
+        String serverHost = "localhost"; // Replace with the actual server host name or IP address
+        int serverPort = 12345; // Replace with the actual server port number
+        int clientId = 1; // Replace with the actual client ID
+        int numConnections = 2; // Replace with the actual number of connections
+
+        try {
+            ClientUDP client = new ClientUDP(serverHost, serverPort);
+            client.downloadFile(clientId, numConnections);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
-}
-
-
