44744dec146e36c3372cd35ce9a846ed527e67a0
Marco Zuliani <m.zuliani@uniandes.edu.co>
2023-03-31 23:58:48 -0500

    Server v2

---
+++ Servidor/UDPServer.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.net.*;
+
+public class UDPServer {
+    public static void main(String[] args) throws IOException {
+        // Leer la entrada del usuario para determinar qué archivo enviar
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("¿Qué archivo desea enviar? (1 o 2)");
+        String fileChoice = reader.readLine();
+        String filename = "archivo" + fileChoice + ".txt";
+        
+        // Configurar el socket UDP
+        DatagramSocket serverSocket = new DatagramSocket(5000);
+        byte[] sendData = new byte[1024];
+        
+        while (true) {
+            // Esperar conexiones de clientes y enviar el archivo
+            byte[] receiveData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            
+            InetAddress clientAddress = receivePacket.getAddress();
+            int clientPort = receivePacket.getPort();
+            
+            FileInputStream fileInputStream = new FileInputStream(filename);
+            int fileSize = fileInputStream.available();
+            int numReads = (int) Math.ceil((double) fileSize / (double) sendData.length);
+            
+            for (int i = 0; i < numReads; i++) {
+                int bytesRead = fileInputStream.read(sendData);
+                DatagramPacket sendPacket = new DatagramPacket(sendData, bytesRead, clientAddress, clientPort);
+                serverSocket.send(sendPacket);
+            }
+            fileInputStream.close();
+        }
+    }
+}
