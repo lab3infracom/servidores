a1350e55a83c402a30e049e1bc956130648db746
Marco Zuliani <m.zuliani@uniandes.edu.co>
2023-04-01 20:59:29 -0500

    Update Buffer.java

--- Servidor/Buffer.java
+++ Servidor/Buffer.java
@@ -1,9 +1,11 @@
 public class Buffer {
     private static int numClientes=0;
 
-    public synchronized void conectar(){  
+    public synchronized void conectar() {  
+        System.out.println("Conectando cliente");
         if (numClientes == 25) {
             try {
+                System.out.println("Esperando a que se desconecte un cliente");
                 wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -12,7 +14,8 @@ public class Buffer {
         numClientes++;
     }
 
-    public synchronized void desconectar(){
+    public synchronized void desconectar() {
+        System.out.println("Desconectando cliente");
         numClientes--;
         notify();
     }
